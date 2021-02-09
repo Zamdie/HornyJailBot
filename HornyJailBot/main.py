6d39a3e925bbd68fb1989f06c372649c26a793b4
Zamdie <74075188+Zamdie@users.noreply.github.com>
2021-02-08 22:16:46 -0300

    Added ability to reply to mentions, added more comments explaining stuff

--- HornyJailBot/main.py
+++ HornyJailBot/main.py
@@ -1,17 +1,20 @@
-import time; # Imports time lib
-from HornyJailBot import HornyJailBot # Imports HornyJailBot class from HornyJailBot lib
+from time import sleep # Imports sleep method from time lib
+from HornyJailBot import HornyJailBot; # Imports HornyJailBot class from HornyJailBot lib
 
-HornyExterminator = HornyJailBot() # I meant "local HornyExterminator = HornyJailBot.new()"
+HornyExterminator = HornyJailBot(); # I meant "local HornyExterminator = HornyJailBot.new()"
 
 try: # Try statement so there is a way to detect program termination
 
-    while True: # Infinite loop
+    while True: # Infinite loop     
+    
+        #HornyExterminator.CheckSubmissions(); # Calls CheckSubmissions method of HornyJailBot object
 
-        time.sleep(15) # Waits 15 seconds
-
-        HornyExterminator.CheckSubmissions() # Calls CheckSubmissions method of HornyJailBot object
+        HornyExterminator.CheckInbox(); # Calls CheckInbox method of HornyJailBot object
         
-finally:
+        sleep(2); # Waits 15 seconds
+        
+except:
 
     # This code will be run after manually stopping execution (Ctrl + C) because it makes things error
+
     HornyExterminator.OnTermination(); # Calls OnTermination method of HornyJailBot object
